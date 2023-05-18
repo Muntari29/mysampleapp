c62a28000437db62f7142893910bde6b07ad1720
Tari <muntrock@gmail.com>
2023-05-18 15:13:35 +0900

    WE 영역 스타일 개선

--- src/components/Contents/Contents.tsx
+++ src/components/Contents/Contents.tsx
@@ -9,9 +9,19 @@ const Contents = () => {
 				<span>2022.06 - 2023.05</span>
 			</div>
 			<div className="flex flex-col gap-1">
-				<HText type="h3">모바일 어플리케이션 개발</HText>
+				<HText type="h4">모바일 어플리케이션 개발</HText>
 				<span>2023.01 - 2023.04</span>
-				<span>2022.06 - 2023.05</span>
+				<HText type="h5">Description</HText>
+				<p>내가 수행한 역할</p>
+				<HText type="h5">Role</HText>
+				<ul className="list-inside list-disc">
+					<li>역할1</li>
+					<li>역할2</li>
+					<li>역할3</li>
+					<li>역할4</li>
+				</ul>
+				<HText type="h5">Tech Stack</HText>
+				<p>모바일 개발</p>
 			</div>
 		</div>
 	);
--- src/components/HText/HText.tsx
+++ src/components/HText/HText.tsx
@@ -3,15 +3,21 @@ import { HTextProps } from './types';
 const HText = ({ type, children, className = '' }: HTextProps) => {
 	switch (type) {
 		case 'h5':
-			return <h5 className={`text-lg ${className}`}>{children}</h5>;
+			return <h5 className={`text-lg font-medium ${className}`}>{children}</h5>;
 		case 'h4':
-			return <h4 className={`text-xl ${className}`}>{children}</h4>;
+			return <h4 className={`text-xl font-medium ${className}`}>{children}</h4>;
 		case 'h3':
-			return <h3 className={`text-3xl ${className}`}>{children}</h3>;
+			return (
+				<h3 className={`text-2xl font-semibold ${className}`}>{children}</h3>
+			);
 		case 'h2':
-			return <h2 className={`text-2xl ${className}`}>{children}</h2>;
+			return (
+				<h2 className={`text-3xl font-semibold ${className}`}>{children}</h2>
+			);
 		default:
-			return <h1 className={`text-4xl ${className}`}>{children}</h1>;
+			return (
+				<h1 className={`text-4xl font-semibold ${className}`}>{children}</h1>
+			);
 	}
 };
 
