47a62820f10937d4d3cf5e6362251eaa7f27e957
seunghee mun <65607601+Muntari29@users.noreply.github.com>
2023-05-22 23:44:25 +0900

    formatToObject Nodejs 로직 추가

--- get-resume-data.js
+++ get-resume-data.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const fetch = require("axios");
 
-const clean = (str) => {
-  const res = str.trim().replace(/\r/g, "");
+const formatToObject = (str) => {
+  // 스프레드 시트의 CSV 파일로 가져온 데이터를 분리하여 사용.
+  const dataList = str.split("\r\n");
+  const res = {};
+  for (let i = 1; i < dataList.length; i++) {
+    const [key, value] = dataList[i].split(",");
+    res[key] = value;
+  }
+  console.log(res);
   return res;
 };
 
 const URL =
-  "https://docs.google.com/spreadsheets/d/1NJV7BGxNxt2qliE6fuKYXoIGLf-FeidQKDRCq3uPlvM/export?format=csv";
+  // "https://docs.google.com/spreadsheets/d/1OYMSlIgjbmzf0OBME_IhhQgeCH7wP52E3QNKc7b2xV0/export?format=csv";
+
+  "https://docs.google.com/spreadsheets/d/1iDBSJ0eyTs3i4NWOc0sC6uyd0niEkwJynoWWLSMCa1E/export?format=csv";
 
 fetch(URL)
   .then((res) => res.data)
-  .then((res) => console.log(res, 2222222222));
+  .then(formatToObject);
