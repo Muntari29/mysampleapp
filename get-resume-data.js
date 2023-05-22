18d2d95a127aa2664aad435d51b52db116417a04
seunghee mun <65607601+Muntari29@users.noreply.github.com>
2023-05-23 00:53:43 +0900

    로컬 서버 및 빌드시 스크립트 명령어 추가

--- get-resume-data.js
+++ get-resume-data.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 // .env 파일 로드 + Node 환경에서 가능한 env.local을 사용하도록 설정함
 dotenv.config({ path: ".env.local" });
+console.log("start get-resume-data Node");
 
 const formatToObject = (str) => {
   // 스프레드 시트의 CSV 파일로 가져온 데이터를 분리하여 사용.
