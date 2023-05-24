db9d0e38d87db63ce05a3e77e87675ebe0dc9fab
seunghee mun <65607601+Muntari29@users.noreply.github.com>
2023-05-24 23:22:49 +0900

    API Routes 워닝 에러 픽스

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  output: "export",
+  // output: "export" 속성은 정적 웹의 크기를 줄이고 웹 서버에 배포하는 속도를 높이고 싶을 때 사용
+  // 워닝을 없애기 위해 빌드 및 prod에서만 속성을 추가
+  output: isProd ? "export" : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true,
