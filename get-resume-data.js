73a9a67c725c49c7212e3dac75d3d7c9d14bb26b
seunghee mun <65607601+Muntari29@users.noreply.github.com>
2023-05-24 23:46:56 +0900

    스프레드 시트 실사용 데이터 관련 Node 로직 개선

--- get-resume-data.js
+++ get-resume-data.js
@@ -8,10 +8,12 @@ console.log("start get-resume-data Node");
 
 const formatToObject = (str) => {
   // 스프레드 시트의 CSV 파일로 가져온 데이터를 분리하여 사용.
-  const dataList = str.split("\r\n");
+  const dataList = str.replace(/"/g, "").split("\r\n");
+
   const res = {};
   for (let i = 1; i < dataList.length; i++) {
-    const [key, value] = dataList[i].split(",");
+    const [key, value] = dataList[i].split(",,");
+
     res[key] = value;
   }
   return res;
