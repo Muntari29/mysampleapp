7b14332f1c380bab86232a8390db197e6ff82406
Tari <muntrock@gmail.com>
2023-05-24 17:59:17 +0900

    scrollToTop 버튼 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,28 +6,17 @@ import Section from '@/components/Section/Section';
 import Contents from '@/components/Contents/Contents';
 import Row from '@/components/Row/Row';
 import ButtonTable from '@/components/ButtonTable/ButtonTable';
-import { BsArrowUpCircleFill } from 'react-icons/bs';
+import { AiOutlineArrowUp } from 'react-icons/ai';
+import IconButton from '@/components/IconButton/IconButton';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	const handleClickTopButton = () => {};
+	const handleClickTopButton = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
 
 	return (
-		// <div className="text-dark-primary dark:text-white  m-auto w-80 ">
-		//   <div className=" bg-dark-primary dark:bg-white rounded-2xl">
-		// <Image
-		//   src={"/ryan.gif"}
-		//   alt={"ryan gif..."}
-		//   width={500}
-		//   height={500}
-		//   priority
-		// />
-		//   </div>
-		//   <div className="text-center p-6">
-		//     <p>CDN 정적 웹 배포하볼까나~</p>
-		//   </div>
-		// </div>
 		<>
 			<main className="grid grid-cols-1 md:grid-cols-[120px_minmax(0,_1fr)] ">
 				<div>
@@ -58,14 +47,11 @@ export default function Home() {
 				<HText type="h2">Contact</HText>
 				<ButtonTable />
 			</Section>
-			<div className="fixed bottom-4 right-4">
-				<button>
-					<BsArrowUpCircleFill
-						size={32}
-						color="#222"
-					/>
-				</button>
-			</div>
+			<IconButton
+				className="fixed bottom-4 right-4"
+				onClick={handleClickTopButton}>
+				<AiOutlineArrowUp size={24} />
+			</IconButton>
 		</>
 	);
 }
